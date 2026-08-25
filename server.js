a6d4d7b0370ea35450d072419400df8a892b3b0e
6947dastanbek-lang <6947dastanbek@gmail.com>
2026-08-25 18:08:30 +0900

    forma va EJS qo'shildi

--- server.js
+++ server.js
@@ -18,12 +18,20 @@ app.set("views", "views"); //
 app.set("view engine", "ejs"); //ejs ekenin korsetip atirmiz
 
 //4  ROUTING CODE ✅
-app.get("/hello", function (req, res) {
-    res.end("<h1>HELLO WORLD BY DANNY</h1>");
-    // res.end(`<h1 style="background: red">HELLO WORLD BY DANNY</h1>`); TEST
-});
-app.get("/gift", function (req, res) {
-    res.end("<h1> Siz sawgalar bolimindesiz </h1>");
+// app.get("/hello", function (req, res) {
+//     res.end("<h1>HELLO WORLD BY DANNY</h1>");
+//     // res.end(`<h1 style="background: red">HELLO WORLD BY DANNY</h1>`); TEST
+// });
+// app.get("/gift", function (req, res) {
+//     res.end("<h1> Siz sawgalar bolimindesiz </h1>");
+// });     //🛑🛑TEST🛑🛑
+// app.post("/create-item", (req, res) => {
+//     console.log(req.body); //req.body 🛑TEST🛑
+//     res.json({ test: "success" });
+// });
+
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app); // core modelimiz  createServer app qabul qiladi ecp past qildiq
